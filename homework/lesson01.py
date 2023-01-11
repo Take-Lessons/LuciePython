b47ed9f77ed900c504202d585d0ad2a1fa5bb195
BeeDev Classroom <38484069+beedev24@users.noreply.github.com>
2023-01-11 21:15:48 +0000

    updated files to run from console

--- homework/lesson01.py
+++ homework/lesson01.py
@@ -1,27 +1,28 @@
-animals = ['Anteater', 'Elephant', 'Tiger', 'Bear']
+def lesson01():
+  animals = ['Anteater', 'Elephant', 'Tiger', 'Bear']
 
-# Print the whole list
-print(animals)
-# Print just the Tiger (remember it starts at 0)
-print(animals[2])
-# Add Panther to the list and print the whole list
-animals.append('Panther')
-print(animals)
-
-zoo = [
-  {
-  "name": "Bronks Zoo",
-  "Location": "NYC",
-  }, 
-  {
-  "name": "Lucies Zoo",
-  "Loation": "NJ"
-  }
-]
-
-# Print the whole list
-print(zoo)
-# Print the 1st Dictionary
-print(zoo[0])
-# Print Lucies Zoo
-print(zoo[1]["name"])
+  # Print the whole list
+  print(animals)
+  # Print just the Tiger (remember it starts at 0)
+  print(animals[2])
+  # Add Panther to the list and print the whole list
+  animals.append('Panther')
+  print(animals)
+  
+  zoo = [
+    {
+    "name": "Bronks Zoo",
+    "Location": "NYC",
+    }, 
+    {
+    "name": "Lucies Zoo",
+    "Loation": "NJ"
+    }
+  ]
+  
+  # Print the whole list
+  print(zoo)
+  # Print the 1st Dictionary
+  print(zoo[0])
+  # Print Lucies Zoo
+  print(zoo[1]["name"])
--- main.py
+++ main.py
@@ -1,67 +1,80 @@
-# variables - a way to define something that you can update later
-#cake = 'Chocolate'
+from homework.lesson01 import *
+from homework.lesson02 import *
 
-# print('*1',cake)
+def main():
+  # variables - a way to define something that you can update later
+  #cake = 'Chocolate'
 
-cake = 'Butter Cream'
+  # print('*1',cake)
 
-# print('*2',cake)
+  cake = 'Butter Cream'
 
-# print(f'Lucie likes {cake} cake') # This way allows us to type a little more naturally
-# print("Lucie likes", cake, "cake")
+  # print('*2',cake)
+  
+  # print(f'Lucie likes {cake} cake') # This way allows us to type a little more naturally
+  # print("Lucie likes", cake, "cake")
+  
+  # The ' ' around chocolate means it is a string
 
-# The ' ' around chocolate means it is a string
+  age = 10 # numbers dont need ' ' 
+  # print(f'Lucie likes {cake} cake and she is {age}')
+  
+  # Booleans - True or False
+  
+  python = True
+  
+  # print(f'Lucie likes {cake} cake and she is {age}. Is she learning Python - {python}')
 
-age = 10 # numbers dont need ' ' 
-# print(f'Lucie likes {cake} cake and she is {age}')
+  # List Tuple Dictionary
+  aList = [1,2,3,4] # is updatable (or mutable)
+  aTuple = (1,2,3,4) # is not updatable (or immutable)
+  
+  aList.append(5)
+  
+  # Key / Value pair
+  aDict = {
+      "anteater": "A animal",
+      "elephant": "Big Gray Animal"
+  }
+  # print('*3',aDict)
+  # print('*4', aDict['anteater'])
+  # print('*5', aDict['elephant'])
 
-# Booleans - True or False
+  animals = [
+      {"anteater": "A animal"},
+      {"elephant": "Big Gray Animal"}
+  ]
+  # print(animals[0]['anteater'])
+  # print(animals[1]['elephant'])
+  
+  ADucktionary = {'Duck' : 'A fluffy animal'}, {'jygrehugij' : 'A bunch of letters'}
+  print(ADucktionary[0])
+  print(ADucktionary[0]['Duck'])
+  print(ADucktionary[0])
 
-python = True
+  house = [
+    {'room1': [{'circle': 'circle'},{'percent': 'percent'}]},
+    {'room2': [{'perenthisis': 'perenthisis'},{'comma': 'comma'}]},
+    {'room3': [{'square': 'square'},{'triangle': 'triangle'}]},
+    {'room4': [{'period': 'period'},{'rectangle': 'rectangle'}]},
+    {'room5': [{'heart': 'heart'}]},
+    {'room6': [{'exclamation point': 'exclamation point'}]},
+    {'room7': [{'question mark': 'question mark'}]},
+  ]
+  # Print the contents of the whole house
+  print(house)
+  
+  # Print the contents of room 6
+  print(house[5])
+  
+  # Print the triangle
+  print(house[2]['room3'][1]['triangle'])
 
-# print(f'Lucie likes {cake} cake and she is {age}. Is she learning Python - {python}')
 
-# List Tuple Dictionary
-aList = [1,2,3,4] # is updatable (or mutable)
-aTuple = (1,2,3,4) # is not updatable (or immutable)
+def lessons():
+  lesson01()
 
-aList.append(5)
 
-# Key / Value pair
-aDict = {
-    "anteater": "A animal",
-    "elephant": "Big Gray Animal"
-}
-# print('*3',aDict)
-# print('*4', aDict['anteater'])
-# print('*5', aDict['elephant'])
-
-animals = [
-    {"anteater": "A animal"},
-    {"elephant": "Big Gray Animal"}
-]
-# print(animals[0]['anteater'])
-# print(animals[1]['elephant'])
-
-ADucktionary = {'Duck' : 'A fluffy animal'}, {'jygrehugij' : 'A bunch of letters'}
-print(ADucktionary[0])
-print(ADucktionary[0]['Duck'])
-print(ADucktionary[0])
-
-house = [
-  {'room1': [{'circle': 'circle'},{'percent': 'percent'}},
-  {'room2': [{'perenthisis': 'perenthisis'},{'comma': 'comma'}]},
-  {'room3': [{'square': 'square'},{'triangle': 'triangle'}]},
-  {'room4': [{'period': 'period'},{'rectangle': 'rectangle'}},
-  {'room5': [{'heart': 'heart'}]},
-  {'room6': [{'exclamation point': 'exclamation point'}]},
-  {'room7': [{'question mark': 'question mark'}]},
-]
-# Print the contents of the whole house
-print(house)
-
-# Print the contents of room 6
-print(house[5])
-
-# Print the triangle
-print(house[2]['triangle'])
+if __name__ == "__main__":
+    # main()
+  lessons()
